8df846b93687b296d11e0900460ff225b9229ad6
Alec Greenaway <aag3@pdx.edu>
2024-01-25 19:30:39 -0800

    fixed javascript not enabled error

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 // src/App.js
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Sidebar from './Sidebar';
-import HomePage from './HomePage';
-import ProductsPage from './ProductsPage';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import HomePage from "./HomePage";
+import ProductsPage from "./ProductsPage";
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Sidebar />
-                <Routes>
-                    <Route path="/" exact component={HomePage} />
-                    <Route path="/products" component={ProductsPage} />
-                    {/* Add more routes as needed */}
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductsPage />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
